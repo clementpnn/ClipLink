9786959a4e0d3a106197fd74eed290b151595393
Clément <95907390+clementpnn@users.noreply.github.com>
2023-11-28 22:07:32 +0100

    add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 
-ReactDOM.createRoot(document.querySelector("#root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.querySelector("#root")!).render(<App />)
