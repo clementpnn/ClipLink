6b2be06f4d9ca5c7a3bcb366ad23fbe037eb9578
Clément <95907390+clementpnn@users.noreply.github.com>
2023-11-29 15:09:59 +0100

    add .env and setup IntelliSense for TypeScript

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_TOKEN: string
+  readonly VITE_GROUP: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
