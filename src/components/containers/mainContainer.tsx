85e5ae5f9ea0508075d827628d5e31e225564c91
Clément <95907390+clementpnn@users.noreply.github.com>
2023-11-30 22:46:12 +0100

    add MainContainner component

---
+++ src/components/containers/mainContainer.tsx
@@ -0,0 +1,3 @@
+export default function MainContainer({ children }: { children: React.ReactNode }) {
+  return <div className="min-h-screen mx-auto max-w-7xl p-4 sm:px-6 lg:px-8">{children}</div>
+}
