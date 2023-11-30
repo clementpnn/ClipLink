95fec7f3c7641f5b0649f7be0be481951cb0a54b
Clément <95907390+clementpnn@users.noreply.github.com>
2023-11-30 22:43:48 +0100

    add svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import path from "node:path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-
+import svgr from "vite-plugin-svgr"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line unicorn/prefer-module
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 })
