89f88c54ecf9b9ef6103224ce38fbf8e2b82db36
Clément <95907390+clementpnn@users.noreply.github.com>
2023-12-01 20:39:45 +0100

    add not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,15 @@
+import { Link } from "@tanstack/react-router"
+
+import { Button } from "@/components/ui/button"
+
 export default function NotFound() {
-  return <div>NotFound</div>
+  return (
+    <div className="h-page-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-4">404</h1>
+      <h3 className="text-2xl font-semibold tracking-tight mb-8">Page Not Found</h3>
+      <Button>
+        <Link to="/">Go to home</Link>
+      </Button>
+    </div>
+  )
 }
